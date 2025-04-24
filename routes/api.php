9844aaa7e97e6665904616461c7d6a9f500bfa3c
Eddy <ehoxsas@gmail.com>
2025-04-24 14:50:21 +0200

    commit api.php

---
+++ routes/api.php
@@ -0,0 +1,41 @@
+<php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CursoController;
+
+// CURSOS
+Route::get('/cursos', [CursoController::class, 'index']);
+Route::get('/cursos/{id}', [CursoController::class, 'show']);
+Route::post('/cursos', [CursoController::class, 'store']);
+Route::put('/cursos/{id}', [CursoController::class, 'update']);
+Route::delete('/cursos/{id}', [CursoController::class, 'destroy']);
+
+
+
+use App\Http\Controllers\ArchivoController;
+
+//ARCHIVOS
+Route::get('/archivos',[ArchivoController::class, 'index']);
+Route::get('/archivos/{id}',[ArchivoController::class, 'show']);
+Route::post('/archivos',[ArchivoController::class, 'store']);
+Route::put('/archivos/{id}',[ArchivoController::class,'update']);
+Route::delete('/archivos/{id}',[ArchivoController::class, 'destroy']);
+
+
+use App\Http\Controllers\ExpedienteController;
+
+//EXPEDIENTES
+Route::get('/expedientes',[ExpedienteController::class, 'index']);
+Route::get('/expedientes/{id}',[ExpedienteController::class, 'show']);
+Route::post('/expedientes',[ExpedienteController::class, 'store']);
+Route::put('/expedientes/{id}',[ExpedienteController::class,'update']);
+Route::delete('/expedientes/{id}',[ExpedienteController::class, 'destroy'])
+
+
+use App\Http\Controllers\NotificacionController;
+
+//NOTIFICACIONES
+Route::get('/notificaciones'[NotificacionController::class, 'index']);
+Route::get('/notificaciones/{id}',[NotificacionController::class, 'show']);
+Route::post('/notificaciones',[NotificacionController::class, 'store']);
+Route::put('/notificaciones/{id}',[NotificacionController::class, 'update']);
+Route::delete('/notificaciones/{id}',[NotificacionController::class, 'destroy']);
