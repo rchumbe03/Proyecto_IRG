62ffd87845119b5f8723ef3ff70d73565b4dac27
george030403 <georgeascuy777@alumnos.ilerna.com>
2025-05-08 09:08:26 +0200

    notifiaciones admin

--- resources/js/src/main.jsx
+++ resources/js/src/main.jsx
@@ -6,15 +6,16 @@ import LoginForm from './components/LoginForm'; // ✅ correcto
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HeaderAd from './components/HeaderAD';
+import Notificaciones from './components/Notificaciones';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>
-      <HeaderAd/>
-       
+    <div className="pagina">
+      <HeaderAd />
+      <Notificaciones />
       <Footer />
-    </>
+    </div>
   </React.StrictMode>
 );
