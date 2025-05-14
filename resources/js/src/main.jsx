ecf9cb173bf7944abd66988c5c40eadff5ec02b9
george030403 <georgeascuy777@alumnos.ilerna.com>
2025-05-14 09:56:46 +0200

    plataforma incio

--- resources/js/src/main.jsx
+++ resources/js/src/main.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FormLogin from './components/Login/FormLogin.jsx';
 import ListaCursos from './components/ListaCursos/ListaCursos.jsx';
 import Notificaciones from "./components/Notificaciones/Notificaciones.jsx";
-
+import Plataforma from "./components/banner/plataforma.jsx";
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Routes>
             <Route path="/login" element={<FormLogin />} />
             <Route path="/cursos" element={<ListaCursos />} />
             <Route path="/notificaciones" element={<Notificaciones />} />
+            <Route path="/plataforma" element={<Plataforma />} />
         </Routes>
     </BrowserRouter>
 );
